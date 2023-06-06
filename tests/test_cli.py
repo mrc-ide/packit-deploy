82dd394c8f985098854bd2fbd1f0e68952cf4fc2
Alex Hill <alex.hill@gmail.com>
2023-06-06 14:26:20 +0100

    revert test pathname where path not checked

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,8 +9,8 @@ from src.packit_deploy.config import PackitConfig
 
 
 def test_parse_args():
-    res = cli.parse_args(["start", "config/noproxy", "--pull"])
-    assert res[0] == "config/noproxy"
+    res = cli.parse_args(["start", "config/basic", "--pull"])
+    assert res[0] == "config/basic"
     assert res[1] is None
     assert res[2] == []
     args = res[3]
@@ -20,13 +20,13 @@ def test_parse_args():
     assert args.volumes is False
     assert args.network is False
 
-    res = cli.parse_args(["start", "config/noproxy", "--extra=extra.yml"])
+    res = cli.parse_args(["start", "config/basic", "--extra=extra.yml"])
     assert res[1] == "extra.yml"
 
-    res = cli.parse_args(["start", "config/noproxy", "--option=a=x", "--option=b.c=y"])
+    res = cli.parse_args(["start", "config/basic", "--option=a=x", "--option=b.c=y"])
     assert res[2] == [{"a": "x"}, {"b": {"c": "y"}}]
 
-    res = cli.parse_args(["stop", "config/noproxy", "--kill", "--network", "--volumes"])
+    res = cli.parse_args(["stop", "config/basic", "--kill", "--network", "--volumes"])
     args = res[3]
     assert args.action == "stop"
     assert args.pull is False
@@ -34,7 +34,7 @@ def test_parse_args():
     assert args.volumes is True
     assert args.network is True
 
-    res = cli.parse_args(["status", "config/noproxy"])
+    res = cli.parse_args(["status", "config/basic"])
     args = res[3]
     assert args.action == "status"
 
