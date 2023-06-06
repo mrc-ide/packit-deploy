abdd57249059ef1e78186cd49adfdacc6712d19c
Alex Hill <alex.hill@gmail.com>
2023-06-06 14:13:34 +0100

    add config

---
+++ tests/test_config.py
@@ -0,0 +1,30 @@
+import pytest
+from unittest import mock
+
+from src.packit_deploy.config import PackitConfig
+
+
+def test_config_no_proxy():
+    cfg = PackitConfig("config/noproxy")
+    assert cfg.network == "packit-network"
+    assert cfg.volumes["outpack"] == "outpack_volume"
+    assert cfg.container_prefix == "packit"
+
+    assert len(cfg.containers) == 4
+    assert cfg.containers["outpack-server"] == "outpack-server"
+    assert cfg.containers["packit"] == "packit"
+    assert cfg.containers["packit-api"] == "packit-api"
+    assert cfg.containers["packit-db"] == "packit-db"
+
+    assert len(cfg.images) == 4
+    assert str(cfg.images["outpack-server"]) == "mrcide/outpack_server:main"
+    assert str(cfg.images["packit"]) == "mrcide/packit:main"
+    assert str(cfg.images["packit-db"]) == "mrcide/packit-db:main"
+    assert str(cfg.images["packit-api"]) == "mrcide/packit-api:main"
+
+    assert cfg.outpack_demo is True
+    assert cfg.proxy_enabled is False
+    assert cfg.protect_data is False
+
+    assert cfg.packit_db_user == "packituser"
+    assert cfg.packit_db_password == "changeme"
