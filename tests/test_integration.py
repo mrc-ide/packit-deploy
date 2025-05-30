4bbcaef397af52fe23fbabfb85759614258db70c
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-05-30 14:58:03 +0100

    Reformat

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -213,18 +213,12 @@ def test_deploy_with_runner_support():
         cfg = PackitConfig(path)
         api = cfg.get_container("packit-api")
 
+        assert get_env_var(api, "PACKIT_ORDERLY_RUNNER_URL") == b"http://packit-orderly-runner-api:8001\n"
         assert (
-            get_env_var(api, "PACKIT_ORDERLY_RUNNER_URL") ==
-            b"http://packit-orderly-runner-api:8001\n"
+            get_env_var(api, "PACKIT_ORDERLY_RUNNER_REPOSITORY_URL")
+            == b"https://github.com/reside-ic/orderly2-example.git\n"
         )
-        assert (
-            get_env_var(api, "PACKIT_ORDERLY_RUNNER_REPOSITORY_URL") ==
-            b"https://github.com/reside-ic/orderly2-example.git\n"
-        )
-        assert (
-            get_env_var(api, "PACKIT_ORDERLY_RUNNER_LOCATION_URL") ==
-            get_env_var(api, "PACKIT_OUTPACK_SERVER_URL")
-        )
+        assert get_env_var(api, "PACKIT_ORDERLY_RUNNER_LOCATION_URL") == get_env_var(api, "PACKIT_OUTPACK_SERVER_URL")
     finally:
         stop_packit(path)
 
