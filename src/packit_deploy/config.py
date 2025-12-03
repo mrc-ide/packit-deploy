ba7f2be5bd0dc07b2a891949cb85d148d0be5e2f
Paul Liétar <pl2113@ic.ac.uk>
2025-12-03 01:31:12 +0000

    Add support for running multiple instances of Packit on the same host.

--- src/packit_deploy/config.py
+++ src/packit_deploy/config.py
@@ -1,3 +1,4 @@
+import dataclasses
 from dataclasses import dataclass
 from pathlib import Path
 from typing import ClassVar, Optional, Union
@@ -44,6 +45,28 @@ def config_buildable(dat, key: list[str], *, repo: str, root: str) -> Union["Bui
         return constellation.ImageReference(repo, name, tag)
 
 
+APP_HTML_ROOT = "/usr/share/nginx/html"  # from Packit app Dockerfile
+
+
+@dataclass
+class Context:
+    root: str
+    repo: str
+    instance: Optional[str]
+
+    def container_name(self, name: str) -> str:
+        if self.instance is None:
+            return name
+        else:
+            return f"{self.instance}-{name}"
+
+    def volume_id(self, name: str) -> str:
+        if self.instance is None:
+            return name
+        else:
+            return f"{self.instance}/{name}"
+
+
 @dataclass
 class Theme:
     accent: str
@@ -69,17 +92,17 @@ class Branding:
     theme_dark: Optional[Theme]
 
     @classmethod
-    def from_data(cls, dat, *, root: str) -> "Branding":
-        name = config.config_string(dat, ["brand", "name"], is_optional=True)
-        logo = config_path(dat, ["brand", "logo_path"], root=root, is_optional=True)
-        logo_link = config.config_string(dat, ["brand", "logo_link"], is_optional=True)
-        logo_alt_text = config.config_string(dat, ["brand", "logo_alt_text"], is_optional=True)
+    def from_data(cls, dat, key: list[str], *, ctx: Context) -> "Branding":
+        name = config.config_string(dat, [*key, "name"], is_optional=True)
+        logo = config_path(dat, [*key, "logo_path"], root=ctx.root, is_optional=True)
+        logo_link = config.config_string(dat, [*key, "logo_link"], is_optional=True)
+        logo_alt_text = config.config_string(dat, [*key, "logo_alt_text"], is_optional=True)
         if logo_alt_text is None and name is not None:
             logo_alt_text = f"{name} logo"
-        favicon = config_path(dat, ["brand", "favicon_path"], root=root, is_optional=True)
+        favicon = config_path(dat, [*key, "favicon_path"], root=ctx.root, is_optional=True)
 
-        theme_light = Theme.from_data(dat, ["brand", "css", "light"])
-        theme_dark = Theme.from_data(dat, ["brand", "css", "dark"])
+        theme_light = Theme.from_data(dat, [*key, "css", "light"])
+        theme_dark = Theme.from_data(dat, [*key, "css", "dark"])
 
         return Branding(
             name=name,
@@ -165,17 +188,16 @@ class ContainerConfig:
     image: constellation.ImageReference
 
     @classmethod
-    def from_data(cls, dat, key: list[str], *, repo: str, container_name: str) -> "ContainerConfig":
+    def from_data(cls, dat, key: list[str], *, container_name: str, ctx: Context) -> "ContainerConfig":
         return ContainerConfig(
-            container_name=container_name,
-            image=config_ref(dat, key, repo=repo),
+            container_name=ctx.container_name(container_name),
+            image=config_ref(dat, key, repo=ctx.repo),
         )
 
 
 @dataclass
 class PackitAPI:
-    container_name: ClassVar[str] = "packit-api"
-
+    container_name: str
     image: constellation.ImageReference
     management_port: int
     base_url: str
@@ -183,12 +205,14 @@ class PackitAPI:
     auth: Optional[PackitAuth]
     runner_git_url: Optional[str]
     runner_git_ssh_key: Optional[str]
+    default_roles: str
 
     @classmethod
-    def from_data(cls, dat, key: list[str], *, repo: str) -> "PackitAPI":
-        image = config_ref(dat, [*key, "api"], repo=repo)
+    def from_data(cls, dat, key: list[str], *, ctx: Context) -> "PackitAPI":
+        image = config_ref(dat, [*key, "api"], repo=ctx.repo)
         management_port = config.config_integer(dat, [*key, "api", "management_port"], is_optional=True, default=8081)
         base_url = config.config_string(dat, [*key, "base_url"])
+        default_roles = config.config_list(dat, [*key, "default_roles"], is_optional=True, default=[])
 
         cors_allowed_origins = config.config_string(
             dat,
@@ -197,14 +221,15 @@ class PackitAPI:
             default="http://localhost*,https://localhost*",
         )
 
-        if "auth" in dat["packit"] and config.config_boolean(dat, ["packit", "auth", "enabled"]):
-            auth = PackitAuth.from_data(dat, ["packit", "auth"])
+        if config.config_boolean(dat, [*key, "auth", "enabled"], is_optional=True, default=False):
+            auth = PackitAuth.from_data(dat, [*key, "auth"])
         else:
             auth = None
 
         runner_git_url = config.config_string(dat, [*key, "runner", "git", "url"], is_optional=True)
         runner_git_ssh_key = config.config_string(dat, [*key, "runner", "git", "ssh-key"], is_optional=True)
         return PackitAPI(
+            container_name=ctx.container_name("packit-api"),
             image=image,
             management_port=management_port,
             base_url=base_url,
@@ -212,22 +237,28 @@ class PackitAPI:
             auth=auth,
             runner_git_url=runner_git_url,
             runner_git_ssh_key=runner_git_ssh_key,
+            default_roles=default_roles,
         )
 
 
 @dataclass
 class PackitDB:
-    container_name: ClassVar[str] = "packit-db"
+    container_name: str
     image: constellation.ImageReference
     user: str
     password: str
 
     @classmethod
-    def from_data(cls, dat, key: list[str], *, repo: str) -> "PackitDB":
-        image = config_ref(dat, key, repo=repo)
+    def from_data(cls, dat, key: list[str], *, ctx: Context) -> "PackitDB":
+        image = config_ref(dat, key, repo=ctx.repo)
         user = config.config_string(dat, [*key, "user"])
         password = config.config_string(dat, [*key, "password"])
-        return PackitDB(image=image, user=user, password=password)
+        return PackitDB(
+            container_name=ctx.container_name("packit-db"),
+            image=image,
+            user=user,
+            password=password,
+        )
 
     @property
     def jdbc_url(self):
@@ -236,23 +267,35 @@ class PackitDB:
 
 @dataclass
 class OrderlyRunner:
-    container_name_api: ClassVar[str] = "orderly-runner-api"
-    container_name_worker: ClassVar[str] = "orderly-runner-worker"
+    redis: ContainerConfig
+    api: ContainerConfig
+    worker: ContainerConfig
 
-    image: constellation.ImageReference
-    workers: int
+    worker_count: int
     env: dict[str, str]
 
     @classmethod
-    def from_data(cls, dat, key: list[str], *, repo: str) -> "OrderlyRunner":
-        image = config_ref(dat, [*key, "image"], repo=repo)
-        workers = config.config_integer(dat, ["orderly-runner", "workers"])
+    def from_data(cls, dat, key: list[str], *, ctx: Context) -> "OrderlyRunner":
+        image = config_ref(dat, [*key, "image"], repo=ctx.repo)
+        worker_count = config.config_integer(dat, ["orderly-runner", "workers"])
         env = config.config_dict(dat, ["orderly-runner", "env"], is_optional=True, default={})
-        return OrderlyRunner(image=image, workers=workers, env=env)
+
+        redis_image = constellation.ImageReference("library", "redis", "8.0")
+        return OrderlyRunner(
+            redis=ContainerConfig(ctx.container_name("redis"), redis_image),
+            api=ContainerConfig(ctx.container_name("orderly-runner-api"), image),
+            worker=ContainerConfig(ctx.container_name("orderly-runner-worker"), image),
+            worker_count=worker_count,
+            env=env,
+        )
 
     @property
     def api_url(self) -> str:
-        return f"http://{self.container_name_api}:8001"
+        return f"http://{self.api.container_name}:8001"
+
+    @property
+    def redis_url(self) -> str:
+        return f"redis://{self.redis.container_name}:6379"
 
 
 @dataclass
@@ -271,8 +314,8 @@ class Proxy:
     port_https: int
 
     @classmethod
-    def from_data(cls, dat, key: list[str], *, repo: str, root: str) -> "Proxy":
-        image = config_buildable(dat, [*key, "image"], repo=repo, root=root)
+    def from_data(cls, dat, key: list[str], *, ctx: Context) -> "Proxy":
+        image = config_buildable(dat, [*key, "image"], repo=ctx.repo, root=ctx.root)
         hostname = config.config_string(dat, [*key, "hostname"])
         port_http = config.config_integer(dat, [*key, "port_http"])
         port_https = config.config_integer(dat, [*key, "port_https"])
@@ -280,26 +323,106 @@ class Proxy:
         return Proxy(image=image, hostname=hostname, port_http=port_http, port_https=port_https)
 
 
-class PackitConfig:
-    app_html_root = "/usr/share/nginx/html"  # from Packit app Dockerfile
+@dataclass
+class PackitInstance:
+    outpack_server: ContainerConfig
+    packit_app: ContainerConfig
+    packit_api: PackitAPI
+    packit_db: PackitDB
+    brand: Branding
 
+    # The handling of volumes in Constellation is a bit rigid and weird.
+    # - Every volume has an ID that is (generally) a constant.
+    # - We need to give Constellation a map from volume ID to volume name,
+    #   based on the user provided configuration.
+    # - When configuring containers we need to pass in the volume ID, not the
+    #   volume name.
+    #
+    # This is a bit inconvenient for running multiple instances of Packit,
+    # where we can't have a finite set of constant volume IDs. These need
+    # to be generated from the instance name and recorded in the fields below.
+    volume_id_outpack: str
+    volume_id_packit_db: str
+
+    # packit_db_backup is not really needed for much. It was mostly used as
+    # scratch space for maintenance operations.
+    volume_id_packit_db_backup: Optional[str]
+
+    # This is the map from volume ID to volume name, for instance-specific
+    # volumes. It gets merged with other instances later.
+    volumes: dict[str, str]
+
+    @classmethod
+    def from_data(cls, dat, key: list[str], *, ctx: Context) -> "PackitInstance":
+        outpack_server = ContainerConfig.from_data(
+            dat,
+            [*key, "outpack", "server"],
+            container_name="outpack-server",
+            ctx=ctx,
+        )
+        packit_app = ContainerConfig.from_data(dat, [*key, "packit", "app"], container_name="packit", ctx=ctx)
+        packit_api = PackitAPI.from_data(dat, [*key, "packit"], ctx=ctx)
+        packit_db = PackitDB.from_data(dat, [*key, "packit", "db"], ctx=ctx)
+        brand = Branding.from_data(dat, [*key, "brand"], ctx=ctx)
+
+        volume_id_outpack = ctx.volume_id("outpack")
+        volume_id_packit_db = ctx.volume_id("packit_db")
+
+        volumes = {
+            volume_id_outpack: config.config_string(dat, [*key, "volumes", "outpack"]),
+            volume_id_packit_db: config.config_string(dat, [*key, "volumes", "packit_db"]),
+        }
+
+        volume = config.config_string(dat, [*key, "volumes", "packit_db_backup"], is_optional=True)
+        if volume is not None:
+            volume_id_packit_db_backup = ctx.volume_id("packit_db_backup")
+            volumes[volume_id_packit_db_backup] = volume
+        else:
+            volume_id_packit_db_backup = None
+
+        return PackitInstance(
+            outpack_server=outpack_server,
+            packit_app=packit_app,
+            packit_api=packit_api,
+            packit_db=packit_db,
+            brand=brand,
+            volume_id_outpack=volume_id_outpack,
+            volume_id_packit_db=volume_id_packit_db,
+            volume_id_packit_db_backup=volume_id_packit_db_backup,
+            volumes=volumes,
+        )
+
+    @property
+    def outpack_server_url(self) -> str:
+        return f"http://{self.outpack_server.container_name}:8000"
+
+    @property
+    def packit_app_endpoint(self) -> str:
+        return f"{self.packit_app.container_name}:80"
+
+    @property
+    def packit_api_endpoint(self) -> str:
+        return f"{self.packit_api.container_name}:8080"
+
+
+class PackitConfig:
+    # Maps internal volume names to user configured names
+    # In cases where multiple instances are used, the internal names look like
+    # `name/outpack` (See Context.volume_id).
     volumes: dict[str, str]
 
     container_prefix: str
     network: str
     protect_data: bool
-    repo: str
     vault: VaultConfig
 
-    outpack: ContainerConfig
-    packit_app: ContainerConfig
-    packit_api: PackitAPI
-    packit_db: PackitDB
     orderly_runner: Optional[OrderlyRunner]
     proxy: Optional[Proxy]
     acme_config: Optional[AcmeBuddyConfig]
 
-    brand: Branding
+    # The map of instances we host, with the same as the key.
+    # In cases where a single unnamed instance is hosted, the key is None.
+    instances: dict[Optional[str], PackitInstance]
 
     def __init__(self, path, extra=None, options=None) -> None:
         dat = config.read_yaml(f"{path}/packit.yml")
@@ -307,33 +430,23 @@ class PackitConfig:
         self.vault = config.config_vault(dat, ["vault"])
         self.network = config.config_string(dat, ["network"])
         self.protect_data = config.config_boolean(dat, ["protect_data"])
-        self.volumes = {
-            "outpack": config.config_string(dat, ["volumes", "outpack"]),
-            "packit_db": config.config_string(dat, ["volumes", "packit_db"]),
-            "packit_db_backup": config.config_string(dat, ["volumes", "packit_db_backup"]),
-        }
 
         self.container_prefix = config.config_string(dat, ["container_prefix"])
-        self.repo = config.config_string(dat, ["repo"])
 
-        self.outpack_server = ContainerConfig.from_data(
-            dat, ["outpack", "server"], repo=self.repo, container_name="outpack-server"
-        )
-        self.packit_app = ContainerConfig.from_data(dat, ["packit", "app"], repo=self.repo, container_name="packit")
-        self.packit_api = PackitAPI.from_data(dat, ["packit"], repo=self.repo)
-        self.packit_db = PackitDB.from_data(dat, ["packit", "db"], repo=self.repo)
+        self.volumes = {}
+
+        repo = config.config_string(dat, ["repo"])
+        ctx = Context(repo=repo, root=path, instance=None)
 
         if "orderly-runner" in dat:
-            self.orderly_runner = OrderlyRunner.from_data(dat, ["orderly-runner"], repo=self.repo)
+            self.orderly_runner = OrderlyRunner.from_data(dat, ["orderly-runner"], ctx=ctx)
             self.volumes["orderly_library"] = config.config_string(dat, ["volumes", "orderly_library"])
             self.volumes["orderly_logs"] = config.config_string(dat, ["volumes", "orderly_logs"])
         else:
             self.orderly_runner = None
 
-        self.brand = Branding.from_data(dat, root=path)
-
         if "proxy" in dat and config.config_boolean(dat, ["proxy", "enabled"]):
-            self.proxy = Proxy.from_data(dat, ["proxy"], repo=self.repo, root=path)
+            self.proxy = Proxy.from_data(dat, ["proxy"], ctx=ctx)
             self.volumes["proxy_logs"] = config.config_string(dat, ["volumes", "proxy_logs"])
         else:
             self.proxy = None
@@ -344,17 +457,20 @@ class PackitConfig:
         else:
             self.acme_config = None
 
-    @property
-    def outpack_server_url(self) -> str:
-        return f"http://{self.outpack_server.container_name}:8000"
+        instances = config.config_dict(dat, ["instances"], is_optional=True)
+        if instances is not None:
+            self.instances = {
+                name: PackitInstance.from_data(
+                    dat,
+                    ["instances", name],
+                    ctx=dataclasses.replace(ctx, instance=name),
+                )
+                for name in instances.keys()
+            }
+        else:
+            self.instances = {
+                None: PackitInstance.from_data(dat, [], ctx=ctx),
+            }
 
-    @property
-    def redis_url(self) -> str:
-        return "redis://redis:6379"
-
-    @property
-    def redis(self) -> ContainerConfig:
-        return ContainerConfig(
-            container_name="redis",
-            image=constellation.ImageReference("library", "redis", "8.0"),
-        )
+        for instance in self.instances.values():
+            self.volumes.update(instance.volumes)
