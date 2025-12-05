a708280645225a0bf7a3d74e13d9651a5d33059c
Paul Liétar <pl2113@ic.ac.uk>
2025-12-05 14:37:00 +0000

    fix tests

--- tests/test_docker_helpers.py
+++ tests/test_docker_helpers.py
@@ -1,10 +1,12 @@
 import pytest
+from constellation import docker_util
 
 from packit_deploy.docker_helpers import DockerClient, write_to_container
 
 
 @pytest.mark.parametrize("mode", [0o644, 0o666, 0o755])
 def test_write_to_container(mode):
+    docker_util.ensure_image("alpine", "alpine:latest")
     with DockerClient() as cl:
         container = cl.containers.create("alpine:latest", ["stat", "-c", "%a %U", "/hello.txt"])
         try:
